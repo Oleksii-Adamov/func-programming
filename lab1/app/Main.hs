f609235638261d13810ed211f8dd50c3be2f3da3
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2023-12-18 07:41:56 +0200

    working on lab1

--- lab1/app/Main.hs
+++ lab1/app/Main.hs
@@ -2,11 +2,17 @@ import Database.HDBC
 import Database.HDBC.ODBC
 import Data.Time.Clock (UTCTime)
 import Data.Maybe (mapMaybe)
+import Data.Maybe (fromMaybe)
 
 -- Define a class for insertable entities
 class Insertable a where
     insert :: Connection -> a -> IO (Maybe Integer)
 
+-- Define a class for updatable entities
+class Updatable a where
+    update :: Connection -> a -> IO ()
+
+-- Define a class for retrievable entities
 class Retrievable a where
     getById :: Connection -> Int -> IO (Maybe a)
     getAll :: Connection -> IO [a]
@@ -182,6 +188,30 @@ instance Insertable Schedule where
           [[sqlId]] -> return $ Just (fromSql sqlId)
           _         -> return Nothing
 
+instance Retrievable Schedule where
+    getById conn id = do
+        stmt <- prepare conn "SELECT * FROM Schedule WHERE ScheduleID = ?"
+        execute stmt [toSql id]
+        result <- fetchRow stmt
+        return $ maybe Nothing convertToSchedule result
+
+    getAll conn = do
+        stmt <- prepare conn "SELECT * FROM Schedule"
+        execute stmt []
+        results <- fetchAllRows stmt
+        return $ mapMaybe convertToSchedule results
+
+convertToSchedule :: [SqlValue] -> Maybe Schedule
+convertToSchedule [sqlId, sqlTopicID, sqlTeacherID, sqlStartDate, sqlEndDate] =
+  Just Schedule
+          { scheduleID = Just (fromSql sqlId)
+          , scheduleTopicID = fromSql sqlTopicID
+          , scheduleTeacherID = fromSql sqlTeacherID
+          , scheduleStartDate = fromSql sqlStartDate
+          , scheduleEndDate = fromSql sqlEndDate
+          }
+convertToSchedule _ = Nothing
+
 instance Insertable Task where
     insert conn task = do
       stmt <- prepare conn "INSERT INTO Tasks (STUDENTID, TOPICID, TASKDESCRIPTION) VALUES (?, ?, ?)"
@@ -197,13 +227,13 @@ instance Retrievable Task where
         stmt <- prepare conn "SELECT * FROM Tasks WHERE TaskID = ?"
         execute stmt [toSql id]
         result <- fetchRow stmt
-        return $ maybe Nothing (convertToTask . map fromSql) result
+        return $ maybe Nothing convertToTask result
 
     getAll conn = do
         stmt <- prepare conn "SELECT * FROM Tasks"
         execute stmt []
         results <- fetchAllRows stmt
-        return $ mapMaybe (convertToTask . map fromSql) results
+        return $ mapMaybe convertToTask results
 
 convertToTask :: [SqlValue] -> Maybe Task
 convertToTask [sqlId, sqlStudentId, sqlTopicId, sqlDescription, sqlGrade] =
@@ -236,6 +266,28 @@ instance Insertable Material where
           [[sqlId]] -> return $ Just (fromSql sqlId)
           _         -> return Nothing
 
+instance Retrievable Material where
+    getById conn id = do
+        stmt <- prepare conn "SELECT * FROM Materials WHERE MaterialID = ?"
+        execute stmt [toSql id]
+        result <- fetchRow stmt
+        return $ maybe Nothing convertToMaterial result
+
+    getAll conn = do
+        stmt <- prepare conn "SELECT * FROM Materials"
+        execute stmt []
+        results <- fetchAllRows stmt
+        return $ mapMaybe convertToMaterial results
+
+convertToMaterial :: [SqlValue] -> Maybe Material
+convertToMaterial [sqlId, sqlTopicId, sqlTitle, sqlLink] =
+    Just Material
+        { materialID = fromSql sqlId
+        , materialTopicID = fromSql sqlTopicId
+        , materialTitle = fromSql sqlTitle
+        , materialLink = fromSql sqlLink
+        }
+convertToMaterial _ = Nothing
 
 instance Insertable Question where
     insert conn question = do
@@ -247,6 +299,34 @@ instance Insertable Question where
           [[sqlId]] -> return $ Just (fromSql sqlId)
           _         -> return Nothing
 
+instance Retrievable Question where
+    getById conn id = do
+        stmt <- prepare conn "SELECT * FROM Questions WHERE QuestionID = ?"
+        execute stmt [toSql id]
+        result <- fetchRow stmt
+        return $ maybe Nothing convertToQuestion result
+
+    getAll conn = do
+        stmt <- prepare conn "SELECT * FROM Questions"
+        execute stmt []
+        results <- fetchAllRows stmt
+        return $ mapMaybe convertToQuestion results
+
+convertToQuestion :: [SqlValue] -> Maybe Question
+convertToQuestion [sqlId, sqlStudentId, sqlText] =
+    Just Question
+        { questionID = fromSql sqlId
+        , questionStudentID = fromSql sqlStudentId
+        , questionText = fromSql sqlText
+        }
+convertToQuestion _ = Nothing
+
+instance Updatable Question where
+    update conn question = do
+        stmt <- prepare conn "UPDATE Questions SET QuestionText = ? WHERE QuestionID = ?"
+        execute stmt [toSql (questionText question), toSql (fromMaybe 0 $ questionID question)]
+        commit conn
+
 instance Insertable Answer where
     insert conn answer = do
       stmt <- prepare conn "INSERT INTO ANSWERS (QUESTIONID, TEACHERID, ANSWER) VALUES (?, ?, ?)"
@@ -257,22 +337,92 @@ instance Insertable Answer where
           [[sqlId]] -> return $ Just (fromSql sqlId)
           _         -> return Nothing
 
+instance Retrievable Answer where
+    getById conn id = do
+        stmt <- prepare conn "SELECT * FROM Answers WHERE AnswerID = ?"
+        execute stmt [toSql id]
+        result <- fetchRow stmt
+        return $ maybe Nothing convertToAnswer result
+
+    getAll conn = do
+        stmt <- prepare conn "SELECT * FROM Answers"
+        execute stmt []
+        results <- fetchAllRows stmt
+        return $ mapMaybe convertToAnswer results
+
+instance Updatable Answer where
+    update conn answer = do
+        stmt <- prepare conn "UPDATE Answers SET AnswerText = ? WHERE AnswerID = ?"
+        execute stmt [ toSql (answerText answer)
+                     , toSql (fromMaybe 0 $ answerID answer)
+                     ]
+        commit conn
+
+convertToAnswer :: [SqlValue] -> Maybe Answer
+convertToAnswer [sqlId, sqlQuestionId, sqlTeacherId, sqlText] =
+    Just Answer
+        { answerID = fromSql sqlId
+        , answerQuestionID = fromSql sqlQuestionId
+        , answerTeacherID = fromSql sqlTeacherId
+        , answerText = fromSql sqlText
+        }
+convertToAnswer _ = Nothing
+
+runApp :: Connection -> IO ()
+runApp conn = do
+    putStrLn "What would you like to do?"
+    putStrLn "1. Insert a student"
+    putStrLn "2. Get student by ID"
+    putStrLn "3. Get all students"
+    putStrLn "4. Exit"
+    putStrLn "Enter your choice (1/2/3/4): "
+    choice <- getLine
+
+    case choice of
+        "1" -> do
+            putStrLn "Enter student details:"
+            putStrLn "First Name: "
+            firstName <- getLine
+            putStrLn "Last Name: "
+            lastName <- getLine
+            putStrLn "Email: "
+            email <- getLine
+            putStrLn "Password: "
+            password <- getLine
+
+            let newStudent = Student { studentID = Nothing, studentFirstName = firstName, studentLastName = lastName, studentEmail = email, studentPassword = password }
+            insertedId <- insert conn newStudent
+            case insertedId of
+                Just id -> putStrLn $ "Inserted student with ID: " ++ show id
+                Nothing -> putStrLn "Failed to insert student"
+            runApp conn
+
+        "2" -> do
+            putStrLn "Enter student ID: "
+            studentIdStr <- getLine
+            let studentId = read studentIdStr :: Int
+            maybeStudent <- getById conn studentId :: IO (Maybe Student)
+            case maybeStudent of
+                Just student -> putStrLn $ "Student details: " ++ show student
+                Nothing -> putStrLn "Student not found"
+            runApp conn
+
+        "3" -> do
+            students <- getAll conn :: IO [Student]
+            putStrLn "All students: "
+            mapM_ (putStrLn . show) students
+            runApp conn
+
+        "4" -> do
+            putStrLn "Exiting..."
+            disconnect conn
+
+        _ -> do
+            putStrLn "Invalid choice"
+            runApp conn
+
 main :: IO ()
 main = do
+    putStrLn "Welcome to the Induvidial Student Work Database App!"
     conn <- connectODBC "DSN=colabPlatformDS;UID=c##colabplatform;PWD=root"
-
---    let student = Student {studentID = Nothing, studentFirstName = "John4", studentLastName = "Doe", studentEmail = "john@example.com", studentPassword = "mypassword" }
-
-    -- Call insert function for Student
---    id <- insert conn student
---    print id
-    maybeStudent <- getById conn 4 :: IO (Maybe Student)
-    putStrLn $ "Fetched student: " ++ show maybeStudent
-    students <- getAll conn :: IO [Student]
-    putStrLn "All students:"
-    mapM_ (putStrLn . show) students
---    print (getAll conn :: IO [Student])
---    rows <- quickQuery conn "SELECT * FROM STUDENTS" []
---    print rows
-    commit conn
-    disconnect conn
+    runApp conn
